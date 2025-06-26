5ef8fc22fc55831975d450173424d2edc97c20fb
Mayank Dudhatra <mayankdudhatracg@gmail.com>
2025-06-26 11:44:43 +0530

    implement cron.js to schedule daily contest creation

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const dotenv = require('dotenv');
 const {  initDB } = require('./config/db.js'); // ✅ Fix this line
 const rateLimiter = require('./middleware/rateLimiter.js');
 const transactionsRoute = require('./routes/transactionsRoute.js');
+import job from './config/cron.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+if (process.env.NODE_ENV === 'production') job.start(); 
+
 app.use(rateLimiter)
 app.use(express.json());
 
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Server is healthy" });
+})
 
 app.use('/api/transaction', transactionsRoute);
 
